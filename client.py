04e1cf76f01d4dccd61c6df6a9a612add4ab70a1
yunjailee <yunjai.lee.82@gmail.com>
2018-06-11 14:42:34 +0900

    Update client.py

--- client.py
+++ client.py
@@ -4,4 +4,4 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
   data = input()
-  sock.sendto(data.encode(),('themaker.iptime.org',1234))
+  sock.sendto(data.encode(),('127.0.0.1',1234))
